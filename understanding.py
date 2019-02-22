8c0f4bbdeee3d3580eaa554ccd33599dcb5bb0fe
Elvis Dohmatob <gmdopp@gmail.com>
2019-02-22 05:59:59 +0100

    more

--- understanding.py
+++ understanding.py
@@ -10,7 +10,6 @@ import torch.nn.functional as F
 from torch.utils.data import TensorDataset, DataLoader
 
 from sklearn.utils import check_random_state
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import roc_auc_score
 
 from joblib import Parallel, delayed
@@ -21,46 +20,17 @@ from mlp import MLP
 
 
 def generate_overlap(num_samples, gamma=.5, beta=.5, random_state=None):
-    assert 0. < gamma < 1.
+    assert 0. <= gamma <= 1.
     assert 0. < beta < 1.
     rng = check_random_state(random_state)
     x = 2 * rng.rand(num_samples) - 1
     y = (np.sign(x) + 1) / 2
     mask = np.abs(x) <= gamma
-    y_noisy = y.copy()
-    y_noisy[mask] = rng.choice(range(2), size=mask.sum(), p=[beta, 1. - beta])
-    return x[:, None], y, y_noisy
+    y[mask] = rng.choice(range(2), size=mask.sum(), p=[beta, 1. - beta])
+    return x[:, None], y
 
 
-
-args = parse_cmdline_args()
-num_samples = 10000
-n_features = 1
-train_size = .6
-batch_size = args.batch_size
-learning_rate = args.learning_rate
-gamma = .2
-beta = .25
-alphas = np.linspace(0, 1, num=args.num_alphas)
-num_jobs = args.num_jobs
-kinds = ["adversarial", "standard", "boltzmann"]
-
-X, y_clean, y = generate_overlap(num_samples, gamma=gamma, beta=beta)
-(X_train, X_test, y_train_clean, y_test_clean, y_train,
- y_test) = train_test_split(X, y_clean, y, train_size=train_size)
-X_train = torch.DoubleTensor(X_train)
-y_train = torch.LongTensor(y_train.astype(int))
-y_train_clean = torch.LongTensor(y_train_clean.astype(int))
-X_test = torch.DoubleTensor(X_test)
-y_test = torch.LongTensor(y_test.astype(int))
-y_test_clean = torch.LongTensor(y_test_clean.astype(int))
-train_loader = DataLoader(TensorDataset(X_train, y_train),
-                          batch_size=batch_size, shuffle=True)
-test_loader = DataLoader(TensorDataset(X_test, y_test_clean),
-                         batch_size=1, shuffle=True)
-
-
-def run_experiment(alpha, kind):
+def run_experiment(alpha, kind, train_loader):
     net = MLP(n_features, output_dim=2)
     loss_func = lambda y_pred, y: F.binary_cross_entropy_with_logits(
         y_pred, smooth_label(y, alpha, y_pred=y_pred, kind=kind))
@@ -72,13 +42,44 @@ def run_experiment(alpha, kind):
     acc = np.mean(preds == y_test.data.numpy().ravel())
     return alpha, kind, net, acc
 
-results = Parallel(n_jobs=num_jobs)(delayed(run_experiment)(alpha, kind)
+
+# misc
+args = parse_cmdline_args()
+n_features = 1
+num_train_samples = 10000
+num_test_samples = 10000
+batch_size = args.batch_size
+learning_rate = args.learning_rate
+gamma = .2
+beta = .25
+alphas = np.linspace(0, 1, num=args.num_alphas)
+num_jobs = args.num_jobs
+kinds = ["adversarial", "standard", "boltzmann"]
+
+# prepare data
+X_train, y_train = generate_overlap(num_train_samples, gamma=gamma, beta=beta)
+X_test, y_test = generate_overlap(num_test_samples, gamma=0.)
+X_train = torch.DoubleTensor(X_train)
+y_train = torch.LongTensor(y_train.astype(int))
+X_test = torch.DoubleTensor(X_test)
+y_test = torch.LongTensor(y_test.astype(int))
+train_loader = DataLoader(TensorDataset(X_train, y_train),
+                          batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(TensorDataset(X_test, y_test),
+                         batch_size=1, shuffle=True)
+
+
+# run experiments in parallel
+results = Parallel(n_jobs=num_jobs)(delayed(run_experiment)(alpha, kind,
+                                                            train_loader)
                                     for alpha in alphas for kind in kinds)
+
+# gather results
 df = []
 xs = np.linspace(-1, 1, num=1000)
 for alpha, kind, net, acc in results:
     df.append(dict(alpha=alpha, kind=kind, acc=acc))
-    logging.info("%g ==> %g" % (alpha, acc))
+    logging.info("(%g, %s) ==> %g" % (alpha, kind, acc))
     preds = net(torch.tensor(xs[:, None]))
     # plt.figure()
     # plt.plot(xs, 2 * preds.argmax(-1).data.numpy() - 1,
@@ -86,6 +87,7 @@ for alpha, kind, net, acc in results:
     # plt.title("decision boundary: kind=%s, $\\alpha=%g$" % (kind, alpha))
     # plt.tight_layout()
 
+# save / plot results
 df = pd.DataFrame(df)
 out_file = "understanding.csv"
 df.to_csv(out_file)
