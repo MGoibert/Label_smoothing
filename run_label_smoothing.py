9dc1313f2f7c47a035e1597849d2d41d4169f58d
Morgane Goibert <morgane.goibert@gmail.com>
2019-03-18 14:11:21 +0100

    Fix: schedular (during training) and perturbed data shape (during attack)

--- run_label_smoothing.py
+++ run_label_smoothing.py
@@ -80,7 +80,7 @@ class MLPNet(nn.Module):
 class LeNetCifar(nn.Module):
 
     def __init__(self):
-        super(NetCifar, self).__init__()
+        super(LeNetCifar, self).__init__()
         self.conv1 = nn.Conv2d(3, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)
