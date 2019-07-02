b6fb87e29f0732745aed1854314b97fe80d73293
Elvis Dohmatob <gmdopp@gmail.com>
2019-07-02 16:02:19 +0200

    small pandas / seaborn script to plot results from experiments

---
+++ plot_figures.py
@@ -0,0 +1,30 @@
+"""
+Plot experimental results from .csv files generate by run_label_smoothing.py
+
+"""
+import sys
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+sns.set_style("darkgrid")  # XXX use better style, e.g "ggplot" ?
+
+# read files
+df = pd.concat(list(map(pd.read_csv, sys.argv[1:])))
+
+# only look at average accuracy over all classes
+df = df.loc[df.label == df.label.max()]
+
+# avoid long floats in plots
+df.alpha = round(df.alpha, 2)
+df.epsilon = round(df.epsilon, 2)
+
+# choose color palette
+palette = sns.color_palette("YlOrRd", 15)
+
+# do the plotting
+sns.factorplot(data=df, x="epsilon", y="acc", hue="alpha", col="smoothing_method",
+               palette=palette)
+
+# last sip
+plt.tight_layout()
+plt.show()
